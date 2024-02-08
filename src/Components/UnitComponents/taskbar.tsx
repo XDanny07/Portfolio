bd5d01fc818f4c4300523aeafd522f1405975131
Omkar Bangar <ombangar0@gmail.com>
2024-02-08 11:22:01 +0530

    negligible changes

--- src/Components/UnitComponents/taskbar.tsx
+++ src/Components/UnitComponents/taskbar.tsx
@@ -1,7 +1,14 @@
+import windows from "../../assets/windows.png";
+import edge from "../../assets/edge.png";
+import file from "../../assets/file.png";
+import vs from "../../assets/vs.png";
 function Taskbar() {
   return (
-    <div className="taskbar absolute bottom-0 bg-gradient-to-b backdrop-blur-3xl w-full h-[2.9rem]">
-      <button>btn</button>
+    <div className="taskbar flex align-center gap-4 absolute bottom-0 bg-gradient-to-b backdrop-blur-3xl w-full h-[2.9rem]">
+      <img className="" src={windows} alt="" />
+      <img src={file} alt="" />
+      <img src={edge} alt="" />
+      <img src={vs} alt="" />
     </div>
   );
 }
