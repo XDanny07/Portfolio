d1c2485c8b5380ddb542859c609df14df90369ce
Omkar Bangar <ombangar0@gmail.com>
2024-09-19 23:56:10 +0530

    minute loader changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import "./App.css";
 import bsod from "./assets/bsod.png";
-import { lazy, Suspense, useEffect, useState } from "react";
-const Homepage = lazy(() => import("./Components/homepage"));
-const Lockscreen = lazy(() => import("./Components/lockscreen"));
+import { useEffect } from "react";
+import Homepage from "./Components/homepage";
+import Lockscreen from "./Components/lockscreen";
 import Bootloader from "./Components/UnitComponents/bootloader";
+
 function App() {
-  const [showContent, setshowContent] = useState(false);
   useEffect(() => {
     const t = setTimeout(() => {
-      setshowContent(true);
+      const bootHelper = document.querySelector(".boothelper");
+      if (bootHelper) {
+        (bootHelper as HTMLElement).style.display = "none";
+      }
     }, 3000);
     () => clearTimeout(t);
   }, []);
-  if (!showContent) return <Bootloader />;
+
   return (
-    <Suspense fallback={<Bootloader />}>
-      <div className="select-none">
-        <img src={bsod} alt="BSOD" className="bsod" />
-        <Lockscreen />
-        <Homepage />
+    <div className="select-none">
+      <div className="boothelper">
+        <Bootloader />
       </div>
-    </Suspense>
+      <img src={bsod} alt="BSOD" className="bsod" />
+      <Lockscreen />
+      <Homepage />
+    </div>
   );
 }
 
